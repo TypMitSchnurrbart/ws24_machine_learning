9dc636219c7a6910f286d604487349e438207519
TypMitSchnurrbart <mueller.alexan-it19@it.dhbw-ravensburg.de>
2023-10-31 17:03:59 +0000

    T

--- Aufgabe2/Aufgabe2_1.py
+++ Aufgabe2/Aufgabe2_1.py
@@ -21,8 +21,8 @@ def poisson_nll(beta, X, y):
 d = np.load('fishing.npz')
 Xt = d['Xt'] #"livebait","camper","persons","child"
 Xte = d['Xte']
-true_fish = d['yt']
-test_fish = d['yte']
+true_fish_train = d['yt']
+true_test_fish = d['yte']
 
 # Clean dataframe
 data = {
@@ -64,14 +64,14 @@ test_data = pd.DataFrame(test_data)
 # Calculate the weights by hand, solve W = (X.T * X)**-1 * X.T * y
 XT_inverse = inv(np.dot(data.T, data))
 full_transform = np.dot(XT_inverse, data.T)
-weights = np.dot(full_transform, true_fish)
+weights = np.dot(full_transform, true_fish_train)
 
 # Show the found weights
 print(f"Solution of regression by hand:\n{weights}\n\n")
 
 # Now do the same but with SciPy
 X = np.asarray(data)
-Y = np.asarray(true_fish)
+Y = np.asarray(true_fish_train)
 
 model = LinearRegression(fit_intercept=False).fit(X, Y)
 print(f"Solution with SkLearn:\n{model.coef_}\n\n")
@@ -79,16 +79,16 @@ print(f"Solution with SkLearn:\n{model.coef_}\n\n")
 # Compute RMSE / variance / NLL 
 pred_fish = np.matmul(test_data, weights)
 
-RMSE = np.sqrt(np.mean((test_fish - pred_fish)**2))
-variance = np.mean((test_fish - pred_fish)**2)
+RMSE = np.sqrt(np.mean((true_test_fish - pred_fish)**2))
+variance = np.mean((true_test_fish - pred_fish)**2)
 
 # NLL based on Gaussian
-NLL = 0.5 * np.log(2 * np.pi * variance) + 0.5 * np.mean((test_fish - pred_fish)**2) / variance
+NLL = 0.5 * np.log(2 * np.pi * variance) + 0.5 * np.mean((true_test_fish - pred_fish)**2) / variance
 print(f"RMSE:\t\t{RMSE}\nVariance:\t{variance}\nNLL:\t\t{NLL}\n")
 
 
 # Compute mean of pred and real
-mean_true = np.mean(true_fish)
+mean_true = np.mean(true_fish_train)
 mean_pred = np.mean(pred_fish)
 
 percentile025 = mean_pred - 1.96 * RMSE
@@ -111,18 +111,16 @@ num_epochs = 5000
 
 for epoch in range(num_epochs):
     mu = np.exp(np.dot(data, beta))
-    gradient = np.dot(data.T, true_fish - mu)
+    gradient = np.dot(data.T, true_fish_train - mu)
     beta += learning_rate * gradient  
 
-print(gradient)
-
-pred_values = np.dot(test_data, beta)
+pred_values = np.exp(np.dot(test_data, beta))
 
 # Calculate Root Mean Square Error (RMSE) on the test set
-RMSE = np.sqrt(np.mean((pred_values - test_fish) ** 2))
+RMSE = np.sqrt(np.mean((pred_values - true_test_fish) ** 2))
 
 # Calculate the NLL on the test set
-NLL = poisson_nll(beta, test_data, test_fish)
+NLL = poisson_nll(beta, test_data, true_test_fish)
 
 print(f"With Gradient Descent and Poisson:\nRMSE:\t\t{RMSE}\nNLL:\t\t{NLL}\n")
  
